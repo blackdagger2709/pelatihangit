7f68056bb038925b5f52e981e00caebbae564694
myfcibaicode <miqdad.farcha@gmail.com>
2018-03-20 20:24:08 +0700

    a

--- Latihan.java
+++ Latihan.java
@@ -1,6 +1,8 @@
 import java.util.*;
 public Latihan{
   public static void main(String[] args) {
-
+    System.out.println("Hello World");
+    System.out.println("HINA");
+    System.out.println("TES LAGI GAN");
   }
 }
